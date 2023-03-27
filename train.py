2f4cc094a85d2799da69f244ee3716a3bf4b8060
rig <1318825571@qq.com>
2023-03-27 12:39:58 +0800

    revise a bug

--- train.py
+++ train.py
@@ -142,7 +142,7 @@ def validate(loader, model, device, optimizer, scheduler):
 
         val_meters['loss'].update(val_loss, n=X.size(0))
         val_meters['cos'].update(val_cos, n=X.size(0))
-    return val_meters['loss'], val_meters['cos']
+    return val_meters['loss'].avg, val_meters['cos'].avg
 
 
 def train(opts, loader, model, device, optimizer, scheduler):
@@ -174,7 +174,7 @@ def train(opts, loader, model, device, optimizer, scheduler):
             train_meters['cos'].update(trn_cos, n=X.size(0))
         if opts.scheduler == 'Step':
             scheduler.step()
-        return train_meters['loss'], train_meters['cos'].avg
+        return train_meters['loss'].avg, train_meters['cos'].avg
 
 
 
